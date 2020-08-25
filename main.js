70fe38ec91fca606e9f3e3c62e1534f8f7cfbc9e
unknown <mansibisen354@gmail.com>
2020-08-25 09:55:15 +0530

    third commit

--- main.js
+++ main.js
@@ -5,9 +5,7 @@ var count = {
         0:0,1:0,2:0,3:0,5:0,6:0,4:0,7:0,8:0,9:0
     },
     total :0,
-    color:{
-        0:null,1:null,2:null,3:0,5:0,6:0,4:0,7:0,8:0,9:0 
-    }
+    
 }
 var container =document.getElementById("container")
 var innerContainer ={ inner : null}
